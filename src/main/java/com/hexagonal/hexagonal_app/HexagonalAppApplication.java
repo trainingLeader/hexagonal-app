db7489408d2252b35a44e734efd5d01cdcda427a
darksidePardo <jjpardo2002@gmail.com>
2024-08-23 11:31:50 -0500

    feat: :tada: Start Project
    Initial project

---
+++ src/main/java/com/hexagonal/hexagonal_app/HexagonalAppApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal.hexagonal_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexagonal/hexagonal_app/HexagonalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.hexagonal_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
